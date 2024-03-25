338793d06cb6ba872849387c4b6289bc5b394a64
Abdurrahman HacSaid <azazfree1515@gmail.com>
2024-03-26 00:43:21 +0300

    g

--- obj/Debug/net8.0/SonmezERP.AssemblyInfo.cs
+++ obj/Debug/net8.0/SonmezERP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SonmezERP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a88143872f5dd792a8dd1f019c6707a0dc3f84f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ddac344f212d509a5983187ab9f31fa00519247")]
 [assembly: System.Reflection.AssemblyProductAttribute("SonmezERP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SonmezERP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
